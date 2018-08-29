ac12f09f5b0d85809e7b65d67a9e57d56ae732e8
Rafal M <rafalmar@gmail.com>
2018-08-30 00:28:10 +0200

    Nq, Nc, Ny added to table

--- ShallFound.py
+++ ShallFound.py
@@ -66,24 +66,29 @@ class Foot:
 			print(grunt)
 			"""
 	def find_below(self):
-		self.below=self.bh[self.bh['bottom']<self.z]
-		self.below['top'][self.below.index.min()]=self.z
-		self.below['thickness']=self.below['top']-self.below['bottom']
+		self.below=self.bh[self.bh['bottom']<self.z].copy()
+		self.below.iloc[0,self.below.columns.get_loc('top')]=self.z
 		print(self.below)
 	
 	def calculate_drained(self):
-		results=self.below
-		results['Nq']=exp(pi*tan(radians(results['fi'])))*(tan(radians(45+results['fi']/2)))^2 
-		results['Nc']=(results['Nq']-1)/tan(radians(results['fi']))
-		results['Ny']=2*(results['Nq']-1)*tan(radians(results['fi'])) # if delta > fi, means if the base is rough
 		self.Bp=self.B-2*fabs(self.ey)
 		self.Lp=self.L-2*fabs(self.ez)
+		results=self.below
+		results['Nq']=results['fi'].apply(lambda x: exp(pi*tan(radians(x)))*(tan(radians(45+x/2)))**2)
+		results['Nc']=results['Nq'].apply(lambda x: (x-1)/tan(radians(x)))
+		results['Ny']=results.apply(lambda x: 2*(x['Nq']-1)*tan(radians(x['fi'])), axis=1) # if delta > fi, means if the base is rough
+		results['Bp']=self.Bp
+		results['Lp']=self.Lp
 		
+		#SPRAWDZIĆ POWYŻSZE WZORY I ODKOMENTOWAĆ PONIŻSZE
+		
+		"""
+				
 		#tu wstawić do tabeli Bp i Lp dla każdej warstwy zgodnie z przyrostem stopy zastępczej
 		
 		if self.shape=='circle':
 			self.sq=1+sin(radians(results['fi']))
-			self..sy=0.7
+			self.sy=0.7
 		else:
 			self.sq=1+(self.Bp/self.Lp)*sin(radians(results['fi']))
 			self.sy=1-0.3*(self.Bp/self.Lp)
@@ -113,7 +118,8 @@ class Foot:
 					teta=pi-np.arccos(costeta)
 				self.m=ml*cos(teta)**2+mb*sin(teta)**2
 			self.iq=1-(Hnorm/(self.V+self.Bp*self.Lp)) # to musi być kolumna w tabeli zamiast stałej wartości bo zależy od C
-		
+		"""
+		print(results)
 parameters=['gamma', 'Moed', 'fi', 'c']		
 		
 soils=[
@@ -133,11 +139,11 @@ bh_1=[
 		]
 
 teren=0
-posadowienie=-3
+posadowienie=-3.7
 	
 foot1=Foot(typ='foot', shape='rectangle', B=3, L=5, h=0.5, z=posadowienie)
 foot1.add_loads(10, 20, 100, 3, 5, 1, 1)
 foot1.load_BH(parameters, soils, bh_1, teren)
 foot1.apply_fill(kind="from_borehole")
 foot1.find_below()
-foot1.calculate_drained
+foot1.calculate_drained()
